2c91deee14fd0d78340a73a4accfbb0779279762
luck <616646651@qq.com>
2017-06-15 22:42:16 +0800

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log('start')
+
+function ss(){
+  console.log('sss')
+}
+var result=document.getElementById("result");
+var file=document.getElementById("file");
+
+function readAsDataURL(){
+    var file = document.getElementById("file").files[0];
+    if(!/image\/\w+/.test(file.type)){
+        alert("看清楚，这个需要图片！");
+        return false;
+    }
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload=function(e){
+        var result=document.getElementById("result");
+        console.log(this.result)
+        result.innerHTML='<img src="' + this.result +'" alt="" />';
+    }
+}
+
+function fff(e){
+  console.log(e)
+  console.log(111)
+  readAsDataURL()
+}
